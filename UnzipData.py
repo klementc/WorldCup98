abe98759da285edf8327d9832694e3837bff8c70
Clément Courageux-Sudan <clement.courageux-sudan@inria.fr>
2020-11-09 11:51:02 +0100

    switch to ftp and correct mistakes

--- UnzipData.py
+++ UnzipData.py
@@ -4,16 +4,21 @@ import fnmatch
 
 def main():
 
-    print "Start to unzip file"
+    print("Start to unzip file")
 
     pattern = '*.gz'
     root_path = './input'
-    tool_path = '/Users/tcheng/Downloads/benchmark/ita_public_tools/'
+    tool_path = './ita_public_tools/'
+
+    print("make tools")
+    os.system("cd "+tool_path+" && make")
+    print("tools built")
 
     list_dirs = os.walk(root_path)
     for root, dirs, files in list_dirs:
         for f in fnmatch.filter(files, pattern):
-            cmd = 'gzip -dc input/' + f + ' | ' + tool_path + 'bin/recreate ' + tool_path + 'state/object_mappings.sort > output/' + f[:-3] + '.log'
+            cmd = 'gzip -dc '+root_path+'/'+f + ' | ' + tool_path +'bin/recreate ' + tool_path + 'state/object_mappings.sort > '+tool_path+'output/' + f[:-3] + '.log'
+            print(cmd)
             os.system(cmd)
 
 
